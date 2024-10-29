cd05ed7546ebab392ab967babc02a915d4926b1d
AlexanderBadenhorst <alexander.baden15.ab@gmail.com>
2024-10-29 10:57:53 +0200

    updated component "Users"

--- chapter12/hello_hooks/src/App.js
+++ chapter12/hello_hooks/src/App.js
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
 import useFetch from "./useFetch";
-import Users from './Users'//create a button for users
+// import Users from "./Users"; //create a button for users
 
 const App = () => {
   const postsUrl = "https://jsonplaceholder.typicode.com/posts";
   const todosUrl = "https://jsonplaceholder.typicode.com/todos";
+  const usersUrl = "https://jsonplaceholder.typicode.com/users";
   //initial state
   const [requested, setRequested] = useState("");
-  const data = useFetch(requested)
-  
+  const data = useFetch(requested);
+
   //works like componentDidMount() as it prevents unnecessary rendering/re-rendering
   //this makes our page run faster/update state faster
   // useEffect(() => {
@@ -23,7 +24,7 @@ const App = () => {
 
   return (
     <div>
-      <Users />
+      {/* <Users /> */}
       {/*displays data from the posts collection*/}
       <Button variant="link" onClick={() => setRequested(postsUrl)}>
         Posts
@@ -32,12 +33,28 @@ const App = () => {
       <Button variant="link" onClick={() => setRequested(todosUrl)}>
         Todos
       </Button>
+      {/* Button for Users */}
+      <Button variant="link" onClick={() => setRequested(usersUrl)}>
+        Users
+      </Button>
       <br />
       Requested: {requested}
+      {/* this had to be modified with conditional rendering to display different lists based on the requested URL */}
       <ul>
-        {data.map(el=>(
-          <li key={el.id}>{el.title}</li>
-        ))}
+        {requested === postsUrl &&
+          data.map((el) => (
+            <li key={el.id}>{el.title}</li> // For posts
+          ))}
+        {requested === todosUrl &&
+          data.map((el) => (
+            <li key={el.id}>
+              {el.title} - {el.completed ? "Completed" : "Not Completed"}
+            </li> // For todos
+          ))}
+        {requested === usersUrl &&
+          data.map((el) => (
+            <li key={el.id}>{el.name}</li> // For users
+          ))}
       </ul>
     </div>
   );
